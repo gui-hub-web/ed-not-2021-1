b7a1b58d719a4acfd56ae5a5053e861924d6848c
Guilherme Vinícius Cintra Ribeiro <guilherme.ribeiro.vinicius8@gmail.com>
2021-02-12 01:25:45 +0000

    "(11/02) Início da revisão de JS"
    $ git commit -m(11/02) Início da revisão de JS

---
+++ revisão-js/vetores.js
@@ -0,0 +1,23 @@
+//Vetor
+let frutas = ['maçã', 'laranja','pera','uva','mamão','banana']
+console.log(frutas)
+
+// Retirar o último elemento de um vetor
+let ultimaFruta = frutas.pop()
+
+// Vetor alterado
+console.log(frutas)
+console.log(ultimaFruta)
+
+let primeiraFruta = frutas.shift()
+
+console.log(frutas)
+console.log(primeiraFruta)
+
+// Remoção em posição intermediária
+// 1° parâmetro -> a posição de remoção (contagem começa em 0)
+// 2° parâmetro -> a quantidade de elementos a remover
+let terceiraFruta = frutas.splice(2, 1) //SEMPRE retornará um vetor
+
+console.log(frutas)
+console.log(terceiraFruta)
